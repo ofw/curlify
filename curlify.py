000b13291c6021699d683394dcea02646c34f38d
egor.orlov <egor.orlov@ostrovok.ru>
2016-08-15 17:44:14 +0300

    fixed None in body

--- curlify.py
+++ curlify.py
@@ -4,8 +4,9 @@
 def to_curl(request):
     headers = ["'{0}: {1}'".format(k, v) for k, v in request.headers.items()]
     headers = " -H ".join(headers)
+
     command = "curl -X {method} -H {headers} -d '{data}' '{uri}'".format(
-        data=request.body,
+        data=request.body or "",
         headers=headers,
         method=request.method,
         uri=request.url,
