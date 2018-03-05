035d9b9aec210b64edc4a0b335c0c2ecf0ecf98e
Chukwudi Nwachukwu <theichux@gmail.com>
2018-03-05 16:31:17 +0100

    Code improvements
    1. Added ability to uncompress the data that was sent back in case it had been compressed
    2. Added test for showing how to use requests prepared method
    
    Note: previous test example failed in some instances. It couldn't add the data for POST requests.

--- curlify_test.py
+++ curlify_test.py
@@ -29,13 +29,13 @@ def test_ok():
 def test_prepare_request():
     response = requests.Request('GET', "http://google.ru", data={"a": "b"}, cookies={"foo": "bar"},
                                 headers={"user-agent": "UA"})
-    assert curlify.to_curl(response.prepare()) == ("curl -X GET "
-                                                   "-H 'Accept-Encoding: gzip, deflate' "
-                                                   "-H 'Accept: */*' "
-                                                   "-H 'Connection: keep-alive' "
-                                                   "-H 'Content-Length: 3' "
-                                                   "-H 'Content-Type: application/x-www-form-urlencoded' "
-                                                   "-H 'Cookie: foo=bar' "
-                                                   "-H 'user-agent: mytest' "
-                                                   "-d 'a=b' 'http://google.ru/' "
-                                                   "--compressed")
+
+    assert curlify.to_curl(response.prepare()) == (
+        "curl -X GET "
+        "-H 'Content-Length: 3' "
+        "-H 'Content-Type: application/x-www-form-urlencoded' "
+        "-H 'Cookie: foo=bar' "
+        "-H 'user-agent: UA' "
+        "-d 'a=b' 'http://google.ru/' "
+        "--compressed"
+    )
