4c5c52c218cc83191d8d7038106503232277458d
Chukwudi Nwachukwu <theichux@gmail.com>
2018-03-05 15:43:40 +0100

    Code improvements
    1. Added ability to uncompress the data that was sent back in case it had been compressed
    2. Added test for showing how to use requests prepared method
    3. I had to do step 2 because it fail to add the requests data on some examples

--- curlify_test.py
+++ curlify_test.py
@@ -1,17 +1,18 @@
 # coding: utf-8
 
-import curlify
 import requests
+
+import curlify
 
 
 def test_ok():
-    r = requests.get(
+    response = requests.get(
         "http://google.ru",
         data={"a": "b"},
         cookies={"foo": "bar"},
-        headers={"user-agent": "mytest"},
+        headers={"user-agent": "mytest"}
     )
-    assert curlify.to_curl(r.request) == (
+    assert curlify.to_curl(response.request) == (
         "curl -X GET "
         "-H 'Accept-Encoding: gzip, deflate' "
         "-H 'Accept: */*' "
@@ -21,4 +22,20 @@ def test_ok():
         "-H 'Cookie: foo=bar' "
         "-H 'user-agent: mytest' "
         "-d 'a=b' 'http://google.ru/'"
+        " --compressed"
     )
+
+
+def test_prepare_request():
+    response = requests.Request('GET', "http://google.ru", data={"a": "b"}, cookies={"foo": "bar"},
+                                headers={"user-agent": "UA"})
+    assert curlify.to_curl(response.prepare()) == ("curl -X GET "
+                                                   "-H 'Accept-Encoding: gzip, deflate' "
+                                                   "-H 'Accept: */*' "
+                                                   "-H 'Connection: keep-alive' "
+                                                   "-H 'Content-Length: 3' "
+                                                   "-H 'Content-Type: application/x-www-form-urlencoded' "
+                                                   "-H 'Cookie: foo=bar' "
+                                                   "-H 'user-agent: mytest' "
+                                                   "-d 'a=b' 'http://google.ru/' "
+                                                   "--compressed")
